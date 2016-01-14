7529bc1006ba03b5f5909082339aa2f15bf8565e
William Yang <yangzwilliam@gmail.com>
2016-01-14 01:18:01 -0500

    commit before even testing because I'm distracted right now

--- js/controllers/ResumeController.js
+++ js/controllers/ResumeController.js
@@ -29,19 +29,38 @@ app.controller('ResumeController', ['$scope', function($scope){
 	}
 	];
 
-	$scope.languages = "Java, Python, MATLAB, JavaScript, HTML/CSS, Arduino, R";
-	$scope.tools = "Bootstrap, JQuery, AngularJS, MeteorJS, Flask, JavaFX, Eclipse, Android Studio";
+	$scope.CScourses = [
+		"CS 201: Data Structures and Algorithms",
+		"ECE 110: Fundamentals of Electrical and Computer Engineering",
+		"EGR 103: Computational Methods in Engineering",
+		"CS 230: Discrete Math for Computer Science",
+		"CS 250: Computer Architecture",
+		"CS 270: Intro to AI",
+		"CS 308: Software Design/Implementation",
+		"CS 330: Design/Analysis of Algorithms"
+	];
+
+	$scope.STAcourses = [
+		"MATH 212: Multivariable Calculus",
+		"MATH 216: Linear Algebra",
+		"STAT 230: Probability",
+		"STAT 210: Regression Analysis",
+		"STAT 250: Statistics"
+	];
+
+	$scope.languages = "Java, JavaScript, HTML/CSS, Python, MATLAB, R, Scheme";
+	$scope.tools = "Git/Github, Unix, JavaFX, Bootstrap, JQuery, AngularJS, MeteorJS";
 
 	$scope.jobs = [{
-		place: "Kids4Coding",
-		title: "Lead Instructor",
-		description: "I developed lesson plans for and taught two separate summer camp courses to kids in grades 6-9. Topics included HTML, CSS, and JavaScript.",
-		duration: "May 2015 - July 2015"
-	},{
 		place: "Duke University CS Department",
 		title: "Undergraduate Teaching Assistant",
 		description: "I am a teaching assistant for CompSci 201: Data Structures and Algorithms. I lead a recitation section, grade assignments, and host helper hours for the course.",
 		duration: "January 2015 - present"
+	},{
+		place: "Kids4Coding",
+		title: "Lead Instructor",
+		description: "I developed lesson plans for and taught two separate summer camp courses to kids in grades 6-9. Topics included HTML, CSS, and JavaScript.",
+		duration: "May 2015 - July 2015"
 	},{
 		place: "Trinity Technology Services",
 		title: "AV Technician",
